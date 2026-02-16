e9d7b0cc54d39037ca5c9c199bf8babb83b0b354
Aaron Bedward <abedward@aprilavenue.co.uk>
2026-02-16 11:40:17 +0000

    GP-113: capture onboarding reset test + evidence

--- GirlPowerTests/OnboardingCompletionRepositoryTests.swift
+++ GirlPowerTests/OnboardingCompletionRepositoryTests.swift
@@ -11,4 +11,17 @@ final class OnboardingCompletionRepositoryTests: XCTestCase {
         repository.markCompleted()
         XCTAssertTrue(repository.hasCompletedOnboarding)
     }
+
+    func testResetClearsFlag() {
+        let suiteName = "test.resetFlag"
+        let defaults = UserDefaults(suiteName: suiteName)!
+        defaults.removePersistentDomain(forName: suiteName)
+        let repository = UserDefaultsOnboardingCompletionRepository(userDefaults: defaults, key: "flag")
+
+        repository.markCompleted()
+        XCTAssertTrue(repository.hasCompletedOnboarding)
+
+        repository.reset()
+        XCTAssertFalse(repository.hasCompletedOnboarding)
+    }
 }
